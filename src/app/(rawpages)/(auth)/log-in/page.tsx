54800aeb1be4b7d72a68353041bd59985d7d4ecf
Muhammad Husnain <aimahusnain@gmail.com>
2025-03-01 07:35:42 -0800

    Add Navbar to Login and SignUp pages for improved navigation

--- src/app/(rawpages)/(auth)/log-in/page.tsx
+++ src/app/(rawpages)/(auth)/log-in/page.tsx
@@ -3,10 +3,14 @@ import Navbar from "@/components/header";
 
 export default function LoginPage() {
   return (
+    <>
+    <Navbar />
       <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
         <div className="w-full max-w-sm">
           <LoginForm />
         </div>
       </div>
+      </>
+
   );
 }
